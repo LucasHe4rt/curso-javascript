c270d8b02e89f32125dc074851315e9ec4cdafb4
LucasHe4rt <lucassilvabittencourt@protonmail.ch>
2020-02-10 23:57:09 -0300

    Conclusão Fundamentos

---
+++ fundamentos/unario.js
@@ -0,0 +1,10 @@
+let num1 = 1
+let num2 = 2
+
+num1++ // Acrescenta uma unidade ao valor da variavel
+console.log(num1)
+--num1 
+console.log(num1)
+
+console.log(++num1 === num2--)
+console.log(num1 === num2)
